fec82b5fa7d55732c64d0b9e1729cc9274748b1b
psenechal <psenechal@itroom.fr>
2025-12-12 19:10:57 +0100

    change app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import {Menubar} from "./components/menubar";
 import {useState, useEffect} from "react";
 import axios from "axios";
-import {Collaborators} from "./pages/collaborators";
+import Header from "./components/Header/Header";
+import Types from "./pages/Types/Types";
 
 function App() {
-  const [collaborators, setCollaborators] = useState([]);
+    const [collaborators, setCollaborators] = useState([]);
 
-  useEffect(() => {
-    axios.get('/collaborators')
-      .then((response) => {
-        console.log(response.data)
-      })
+    useEffect(() => {
+        axios.get('/collaborators')
+            .then((response) => {
+                console.log(response.data)
+            })
 
-  }, []);
+    }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <Menubar />
-      <Collaborators />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Types />
+        </div>
+    );
 }
 
 export default App;
